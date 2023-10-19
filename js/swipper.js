c776028da85a78efb45e59099dcbac820206c8a1
Jessikoo <147804147+Jessikoo@users.noreply.github.com>
2023-10-19 10:56:11 +0200

    Add files via upload

---
+++ js/swipper.js
@@ -0,0 +1,4 @@
+new Swiper(".swiper", {
+  navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+  pagination: { el: ".swiper-pagination", clickable: true, bulletElement: 'button' }
+});
